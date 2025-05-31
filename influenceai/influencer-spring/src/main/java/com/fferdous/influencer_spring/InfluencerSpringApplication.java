d29ea74b1295e3e18dcb343a8793c4a2b5d84e58
fferdous <fferdous@umass.edu>
2025-05-30 21:33:12 -0400

    implemented backend

---
+++ influenceai/influencer-spring/src/main/java/com/fferdous/influencer_spring/InfluencerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.fferdous.influencer_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfluencerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfluencerSpringApplication.class, args);
+	}
+
+}
