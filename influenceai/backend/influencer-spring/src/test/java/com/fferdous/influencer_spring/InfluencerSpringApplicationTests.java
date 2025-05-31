a078955e21dd44c22e338232ee9e58bb724fb161
fferdous <fferdous@umass.edu>
2025-05-31 10:46:06 -0400

    Connected backend to database

---
+++ influenceai/backend/influencer-spring/src/test/java/com/fferdous/influencer_spring/InfluencerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fferdous.influencer_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfluencerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
